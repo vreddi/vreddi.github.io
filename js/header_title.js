9bd7f7af9a28135fc3572492cadefe6f17bc5876
Vishrut Reddi <vishrutreddi@gmail.com>
2014-09-29 14:39:01 -0500

    Revision 26

---
+++ js/header_title.js
@@ -0,0 +1,21 @@
+setOnetime(function(){
+
+	if ( document.URL.contains("experience.html") ){
+		header_slider.style.display = "block";
+		header_slider.style.float = "left";
+	}
+	else if( document.URL.contains("projects.html")){
+		header_slider.style.display = "block";
+		header_slider.style.float = "left";
+	}
+	else if( document.URL.contains("blog.html")){
+		header_slider.style.display = "block";
+		header_slider.style.float = "left";
+	}
+	else if( document.URL.contains("contact.html")){
+		header_slider.style.display = "block";
+		header_slider.style.float = "left";
+	}
+	
+
+}, 500);
