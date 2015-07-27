0713885c6bda2324da1d59f4f70eaf11531d6366
Vishrut Reddi <vishrutreddi@gmail.com>
2015-07-27 01:39:53 -0700

    About Me Added, Project Section Improved
    * About me Description has been added. With that the Twitter Feed
    height has also been changed and set to 800.
    
    * New carousel for projects has been added. Some units in the carousel
    is empty and the project section needs to be completed.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 // Carousel Auto-Cycle
+// Blog Carousel
   $(document).ready(function() {
     $('.carousel').carousel({
       interval: 10000
     })
   });
+
+// Project Carousel
+// $(document).ready(function() {
+//     $('#Carousel').carousel({
+//         interval: 5000
+//     })
+// });
