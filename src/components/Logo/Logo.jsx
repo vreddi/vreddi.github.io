9aace6f6be52deb1ab12baee0778a01d03032eed
Vishrut Reddi <vishrutreddi@gmail.com>
2019-12-24 22:46:33 -0800

    Resolve merge conflict markers

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import logo0 from '../../assets/L0.png';
 import logo1 from '../../assets/L1.png';
 import logo2 from '../../assets/L2.png';
@@ -29,18 +28,8 @@ class Logo extends React.Component{
   }
 
   render = () => (
-    <img className="logo" src={this.state.image} />
+    <img className="logo" src={this.state.image} alt="" />
   );
 }
-=======
-import './Logo.css';
-
-const Logo = () => (
-  <div class="logo">
-    <div class="logo-title">Vishrut Reddi</div>
-    <div class="logo-sub-title">If can't beat it, eat it.</div>
-  </div>
-);
->>>>>>> 3e11bf9969440e902d658de40e7d0dc3d68e1616
 
 export default Logo;
